2143b60ba68f7f851007baf5c068ac122acc9d76
Raya33 <rayaabudalbouh@gmail.com>
2021-01-06 14:33:33 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use Strict';
+score =0
 var answer = 'yes';
 var aswer = prompt('do you think that my name have story ?');
 if (answer.toLowerCase() === 'yes' || answer.toLowerCase === 'y') {
     alert('good, thats right');
+    score++;
     console.log('good, thats right');
 }
 else {
@@ -14,6 +16,7 @@ var answer2 = 'yes';
 var answer2 = prompt('does my father have the biggest impact on my childhood?');
 if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase === 'y') {
     alert('coool, thats right');
+    score++;
     console.log('coool, thats right');
 }
 else {
@@ -26,6 +29,7 @@ var answer3 = 'yes';
 var answer3 = prompt('Have I ever visited another continent?');
 if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase === 'y') {
     alert('thats right');
+    score++;
     console.log(' thats right');
 }
 else {
@@ -37,6 +41,7 @@ var answer4 = 'yes';
 var answer4 = prompt('Do you think I like horror movies?');
 if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase === 'y') {
     alert('yes , i do');
+    score++;
     console.log('yes , i do');
 }
 else {
@@ -47,38 +52,58 @@ var answer5 = 'yes';
 var aswer5 = prompt('Do you think I like chocolate milkshake?');
 if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase === 'y') {
     alert('great');
+    score++;
     console.log('great');
 }
 else {
     alert('oops');
 }
 
-var answer6 = 24;
-// var opp = [1, 2, 3, 4];
-for (var i = 0; i < 5; i++) {
-    var userAnswer = prompt('guess my Age')
-    if (Number(userAnswer) === answer6) {
-        alert('thats right !');
-        break
-    } else if (Number(userAnswer) < answer6) {
-        alert('too low');
-    } else if (Number(userAnswer) > answer6) {
-        alert('too high!');
-    }
+// var answer6 = 24;
+// // var opp = [1, 2, 3, 4];
+// for (var i = 0; i < 5; i++) {
+//     var userAnswer = prompt('guess my Age')
+//     if (Number(userAnswer) === answer6) {
+//         alert('thats right !');
+//         score++;
+//         break
+//     } else if (Number(userAnswer) < answer6) {
+//         alert('too low');
+//     } else if (Number(userAnswer) > answer6) {
+//         alert('too high!');
+//     }
+// }
+// alert('the correct answer is 24');
+var answer6=24
+switch (Number(answer6)) {
+    case 4:
+        alert('thats correct!');
+        score++;
+        i=5;
+        break;
+    case(Number(answer6)<24);
+        alert('too low!');
+        break;
+    case(Number(answer6)) >24);
+         alert('too high!') ;
+         break;
+    default:
+        alert('inter a number');
+        break;
 }
-alert('the correct answer is 24');
+
 
 
 
 var cars =['Mercedes','FERRARI','PORSCHE','DACIA SANDERO'];
+var answer7=prompt('what is my favorite car?')
 for(var index=0;index<cars.length;index++){
-    var answer7=prompt('what is my favorite car?');
-    if (answer7.toLowerCase()===cars) {
-       alert('thats right!'+answer7+'is one of my favorite cars')
-       break
-    }else{
-        alert('wrong!')
+        if (cars[index]===answer7.toLowerCase()) {
+           alert('correct answer!');
+           score++;
+           break;
     }
 }
+alert(score)
 alert('my favorite cars are Mercede,FERRARI, PORSCHE,DACIA SANDERO')
 confirm('okay thats it ...thank u dear and welcome again!')
